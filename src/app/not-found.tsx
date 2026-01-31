8feb577e0711bb31c803365eb34987196efc5737
Aziz <abdulazeezsalami19@gmail.com>
2026-01-31 19:36:13 +0100

    updated no-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,5 @@
+import { Footer } from "@/components/layout/Footer";
+import Navbar from "@/components/layout/navbar/Navbar.server";
 import { Button } from "@/components/ui/Button";
 import Link from "next/link";
 import { GoHomeFill } from "react-icons/go";
@@ -5,12 +7,16 @@ import { GoHomeFill } from "react-icons/go";
 export default function NotFound() {
 
     return (
-        <div className=" flex-1 flex justify-center items-center flex-col">
-            <h1 className=' text-6xl md:text-9xl font-thin text-gray-500 dark:text-gray-300'>404</h1>
-            <p className=' text-2xl text-gray-500 dark:text-gray-300'>Page Not Found</p>
-            <Link href={"/"} className=" mt-6">
-                <Button text="Home" icon={GoHomeFill} variant="secondary" />
-            </Link>
-        </div>
+        <main className=" flex-1 flex flex-col">
+            <Navbar />
+            <div className=" flex-1 flex justify-center items-center flex-col">
+                <h1 className=' text-6xl md:text-9xl font-thin text-gray-500 dark:text-gray-300'>404</h1>
+                <p className=' text-2xl text-gray-500 dark:text-gray-300'>Page Not Found</p>
+                <Link href={"/"} className=" mt-6">
+                    <Button text="Home" icon={GoHomeFill} variant="secondary" />
+                </Link>
+            </div>
+            <Footer />
+        </main>
     )
 }
