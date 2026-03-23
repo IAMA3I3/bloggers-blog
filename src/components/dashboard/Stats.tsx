a5ea92a535e83beec10606c4f771783aed0f70a7
Aziz <abdulazeezsalami19@gmail.com>
2026-03-23 14:52:42 +0100

    display user count on dashboard

--- src/components/dashboard/Stats.tsx
+++ src/components/dashboard/Stats.tsx
@@ -1,11 +1,29 @@
+import { getCollection } from "@/lib/db";
 import { StatsCard } from "../ui/Cards";
 import { FaUsers, FaBlog, FaBell } from "react-icons/fa"
+import { User } from "@/types/auth";
+import getAuthUser from "@/lib/auth/getAuthUser";
 
-export default function Stats() {
+export default async function Stats() {
+
+    const authUser = await getAuthUser()
+
+    let userCount: number = 0
+
+    try {
+        const userCollection = await getCollection<User>("users")
+        userCount = await userCollection.countDocuments()
+    } catch (err) {
+        throw new Error("Failed to load users")
+    }
 
     return (
         <div className=" flex gap-8 flex-col md:flex-row *:w-full">
-            <StatsCard display icon={<FaUsers />} value={20} text="Users" />
+            {
+                authUser?.role === "admin" && (
+                    <StatsCard display icon={<FaUsers />} value={userCount} text="Users" />
+                )
+            }
             <StatsCard display icon={<FaBlog />} value={12} text="Posts" />
             <StatsCard display icon={<FaBell />} value={23} text="Notifications" />
         </div>
